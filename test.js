e3574568141691d6b406527513712dddb421788d
Amy Lu <doggy0704@gmail.com>
2022-11-16 16:01:52 +0800

    三星瀏覽器抓不到,改抓取方式

--- test.js
+++ test.js
@@ -1,17 +1,19 @@
-const p = document.querySelector('p');
+const p = document.getElementById('p');
 
 console.log(navigator.userAgentData.platform);
 
-if(navigator.userAgentData.platform =='Android'){
-    p.textContent = navigator.userAgentData.platform + navigator.userAgent + navigator.userAgentData.mobile;
-}else if(navigator.userAgentData.platform =='Windows'){
-    p.textContent = navigator.userAgentData.platform + navigator.userAgent + navigator.userAgentData.mobile;
-    console.log(navigator.userAgent);
-}else if(navigator.userAgentData.platform =='iOS'){
-    p.textContent = navigator.userAgentData.platform + navigator.userAgent + navigator.userAgentData.mobile;
-    console.log(navigator.userAgent);
-}else{
-    p.textContent = navigator.userAgentData.platform + navigator.userAgent + navigator.userAgentData.mobile;
-}
+// if(navigator.userAgentData.mobile){
+//     p.textContent = navigator.userAgentData.platform + navigator.userAgent + navigator.userAgentData.mobile;
+// }else if(navigator.userAgentData.platform =='Windows'){
+//     p.textContent = navigator.userAgentData.platform + navigator.userAgent + navigator.userAgentData.mobile;
+//     console.log(navigator.userAgent);
+// }else if(navigator.userAgentData.platform =='iOS'){
+//     p.textContent = navigator.userAgentData.platform + navigator.userAgent + navigator.userAgentData.mobile;
+//     console.log(navigator.userAgent);
+// }else{
+//     p.textContent = navigator.userAgentData.platform + navigator.userAgent + navigator.userAgentData.mobile;
+// }
 
+
+p.textContent = navigator.userAgentData.platform + navigator.userAgent + navigator.userAgentData.mobile;
 
