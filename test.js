72f02a4e5e5d77277febe6bf4610d6092a10aedc
Amy Lu <doggy0704@gmail.com>
2022-11-16 15:00:18 +0800

    修改轉址

--- test.js
+++ test.js
@@ -4,15 +4,12 @@ console.log(navigator.userAgentData.platform);
 
 if(navigator.userAgentData.platform =='Android'){
     p.textContent = navigator.userAgentData.platform + navigator.userAgent + navigator.userAgentData.mobile;
-}else if(!navigator.userAgentData.mobile){
+    location.href = 'https://play.google.com/store/games';
+}else if(navigator.userAgentData.platform =='windows'){
     p.textContent = navigator.userAgentData.platform + navigator.userAgent + navigator.userAgentData.mobile;
+    location.href = 'https://www.google.com/';
     console.log(navigator.userAgent);
 }else if(navigator.userAgentData.platform =='iOS'){
     p.textContent = navigator.userAgentData.platform + navigator.userAgent + navigator.userAgentData.mobile;
     console.log(navigator.userAgent);
 }
-
-let a = 1;
-let b=a+=1;
-
-console.log(b);
